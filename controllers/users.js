8f2dce34383065b253b9017f5e189d51ef95acd6
Alanny <achaverrim@est.utn.ac.cr>
2023-04-07 18:30:21 -0600

    api_zaylin

--- controllers/users.js
+++ controllers/users.js
@@ -1,122 +1,102 @@
-const { request, response } = require('express');
-const Usuario = require('../models/users');
-const Doctor=require('../models/doctor.js')
-var bcrypt = require('bcryptjs');
+const { request, response } = require("express");
+const Usuario = require("../models/users");
+const Doctor = require("../models/doctor.js");
+var bcrypt = require("bcryptjs");
 
 const usersGETPOST = async (req = request, res = response) => {
-    try {
-      const users = await Usuario.find({ 'state': true });
-      res.status(200).json({
-        msg: "Mensaje desde el metodo GET de usuarios",
-        users,
-      });
-    } catch (err) {
-      console.log(err);
-      throw new Error("Error en el metodo GET");
-    }
-  };
-  
-  const usersGET = async (req = request, res = response) => {
-    try {
-      // const rol = { rol: "public", google: true };
-      //const {limit}=req.query;
-      const users = await Usuario.find({ 'state': true });
-      res.render("users", { users });
-    } catch (err) {
-      console.log(err);
-      throw new Error("Error en el metodo GET");
-    }
-  };
+  try {
+    const users = await Usuario.find({ state: true });
+    res.status(200).json({
+      msg: "Mensaje desde el metodo GET de usuarios",
+      users,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error en el metodo GET");
+  }
+};
 
-  
-
-
+const usersGET = async (req = request, res = response) => {
+  try {
+    // const rol = { rol: "public", google: true };
+    //const {limit}=req.query;
+    const users = await Usuario.find({ state: true });
+    res.render("users", { users });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error en el metodo GET");
+  }
+};
 
 const usersPOST = async (req = request, res = response) => {
-
-    try {
-        const { name, email, password, google, role, state } = req.body
-        const user = new Usuario({ name, email, password, google, role, state });
-        var salt = bcrypt.genSaltSync(10);
-        user.password = bcrypt.hashSync(password, salt);
-        await user.save();
-        res.json(
-            {
-                ok: 200,
-                "msg": "Mensaje desde el metodo POST",
-                user
-            }
-        );
-
-    }
-    catch (err) {
-        console.log(err);
-        throw new Error('Error en el metodo POST');
-    }
-}
-
+  try {
+    const { name, email, password, google, role, state } = req.body;
+    const user = new Usuario({ name, email, password, google, role, state });
+    var salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(password, salt);
+    await user.save();
+    res.json({
+      ok: 200,
+      msg: "Mensaje desde el metodo POST",
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error en el metodo POST");
+  }
+};
 
 const usersPUT = async (req = request, res = response) => {
-
-    try {
-        const { id } = req.params;
-        const {password,name, ...resto } = req.body;
-        if (password) {
-            const salt = bcrypt.genSaltSync();
-            resto.password = bcrypt.hashSync(password, salt);
-        }
-        const users = await Usuario.findByIdAndUpdate(id, resto);
-
-        if(users.role.toLowerCase()=='medico'){
-            const user=users._id;
-            const name=users.name
-            const speciality= req.body.speciality? req.body.speciality:"Medicina General";
-            const doctors= new Doctor({name,user,speciality});
-            await doctors.save();
-        }
-       
-
-        res.json(
-            {
-                ok: 200,
-                "msg": "Mensaje desde el metodo PUT",
-                users
-            }
-        );
+  try {
+    const { id } = req.params;
+    const { password, name, ...resto } = req.body;
+    if (password) {
+      const salt = bcrypt.genSaltSync();
+      resto.password = bcrypt.hashSync(password, salt);
     }
-    catch (err) {
-        console.log(err);
-        throw new Error('Error en el metodo PUT');
-    }
-}
+    const users = await Usuario.findByIdAndUpdate(id, resto);
 
+    if (users.role.toLowerCase() == "medico") {
+      const user = users._id;
+      const name = users.name;
+      const speciality = req.body.speciality
+        ? req.body.speciality
+        : "Medicina General";
+      const doctors = new Doctor({ name, user, speciality });
+      await doctors.save();
+    }
+
+    res.json({
+      ok: 200,
+      msg: "Mensaje desde el metodo PUT",
+      users,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error en el metodo PUT");
+  }
+};
 
 const usersDELETE = async (req = request, res = response) => {
-
-    try {
-        const { id } = req.params;
-        //const user= await Usuario.findByIdAndDelete(id);
-        const user = await Usuario.findByIdAndUpdate(id, { 'state': false });
-        res.json(
-            {
-                ok: 200,
-                "msg": "Mensaje desde el metodo DELETE",
-                user
-            }
-        );
-
-    }
-    catch (err) {
-        console.log(err);
-        throw new Error('Error en el metodo DELETE');
-    }
-}
-
+  try {
+    const { id } = req.params;
+    //const user= await Usuario.findByIdAndDelete(id);
+    const user = await Usuario.findByIdAndUpdate(id, { state: false });
+    res.json({
+      ok: 200,
+      msg: "Mensaje desde el metodo DELETE",
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error en el metodo DELETE");
+  }
+};
 
 module.exports = {
-    usersGET,
-    usersPOST,
-    usersPUT,
-    usersDELETE,
-    usersGETPOST
-}
+  usersGET,
+  usersPOST,
+  usersPUT,
+  usersDELETE,
+  usersGETPOST,
+};
--- helpers/db_validates.js
+++ helpers/db_validates.js
@@ -1,5 +1,4 @@
 const Usuario = require("../models/users");
-const Paciente = require("../models/patients");
 
 const db_ExistEmail = async (email = "") => {
   const existEmail = await Usuario.findOne({ email }).exec();
@@ -8,16 +7,6 @@ const db_ExistEmail = async (email = "") => {
     throw new Error("El email ya existe");
   }
 };
-
-const db_ExistDNI = async (dni = "") => {
-  const existDNI = await Paciente.findOne({ dni }).exec();
-  if (existDNI) {
-    console.log("El DNI ya se encuentra asignado a un paciente");
-    throw new Error("El DNI ya se encuentra asignado a un paciente");
-  }
-};
-
 module.exports = {
   db_ExistEmail,
-  db_ExistDNI,
 };
